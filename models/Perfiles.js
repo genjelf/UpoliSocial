44a812e847112c15f6bfbe8561ebe566d3794dd1
genjelf <genjelf09@gmail.com>
2017-09-11 11:11:50 -0600

    Update Perfiles.js
    Nuevos atributos al modelo de perfiles

--- models/Perfiles.js
+++ models/Perfiles.js
@@ -2,14 +2,17 @@ var mongoose = require('mongoose');
 
 var PerfilSchema = new mongoose.Schema({
 	
-	sede: String,
+      sede: String,
       escuela: String,
       carrera: String,
       nombre: String,
       edad:Number,
       anioIngreso:Number,
       anioEgreso:Number,
-      tipoUsuario: String
+      tipoUsuario: String,
+      pass:String
+	
+
 });
 
 mongoose.model('Perfiles', PerfilSchema);
